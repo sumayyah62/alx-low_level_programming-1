408fd73a1d983fdbb6b73781e6e9f082a6a5b031
sumayyah62 <sumayyahadeola@gmail.com>
2022-10-17 14:05:39 -0700

    tasd advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 /**
- * main-program entry point
+ * main - write a c program that prints.
  *
- * Return: 1 on success.
+ * Return: 0
  */
 int main(void)
 {
-write(2, "and that of art is useful\"- Dora Korper, 2015- 10- 19\n", 59);
-return (1);
+	write(2."and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19
+	\n, 59);
+	return (1);
 }
